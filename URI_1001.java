1e1a760ac2dba700914107d3821e75587b498cb9
Mehrab Mohsin Khan <47208766+Abeshkhan@users.noreply.github.com>
2019-02-23 03:14:59 +0600

    Update URI_1001.java

--- URI_1001.java
+++ URI_1001.java
@@ -1,6 +1,6 @@
 
 import java.util.Scanner;
-public class URI_1001{
+public class Main{
   public static void main (String args[]){
  Scanner abesh =new Scanner(System.in);
   System.out.println("Enter First Number");
