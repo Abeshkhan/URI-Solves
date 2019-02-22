02447e7ad957c56872a0567d7a7a4c4651dae5fb
Mehrab Mohsin Khan <47208766+Abeshkhan@users.noreply.github.com>
2019-02-23 02:15:45 +0600

    Add files via upload

---
+++ URI_1001.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class URI_1001{
+  public static void main (String args[]){
+ Scanner abesh =new Scanner(System.in);
+  System.out.println("Enter First Number");
+  int A=abesh.nextInt();
+  System.out.println("Enter second number");
+  int B=abesh.nextInt();
+  int X;
+  X = A + B ;
+  System.out.println(X+"\n");
+                                           
+                                            
+   }
+}                                        
+                                          
