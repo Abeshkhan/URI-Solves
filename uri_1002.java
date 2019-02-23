fb87d1472c14e27c9fa2689a39f85ae331b37399
Mehrab Mohsin Khan <47208766+Abeshkhan@users.noreply.github.com>
2019-02-24 00:40:18 +0600

    Add files via upload

---
+++ uri_1002.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+  public class uri_1002
+{
+    public static void main(String args[])
+    {
+      Scanner abesh=new Scanner(System.in);
+      double R=abesh.nextDouble();
+      double  n = 3.14159;
+      double A =  n * R*R;
+      System.out.printf("A=%.4f",A);
+      System.out.println();
+     
+    }}
+  
+      
