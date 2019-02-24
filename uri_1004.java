d97e1727ceb9f04febfdac5d7972f8424c2d88a6
Mehrab Mohsin Khan <47208766+Abeshkhan@users.noreply.github.com>
2019-02-24 16:12:00 +0600

    Add files via upload

---
+++ uri_1004.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class uri_1004{
+  public static void main(String args[]){
+    Scanner abesh=new Scanner (System.in);
+    int A=abesh.nextInt();
+    int B=abesh.nextInt();
+    int PROD = A*B;
+    System.out.println("PROD = "+PROD);
+  }}
