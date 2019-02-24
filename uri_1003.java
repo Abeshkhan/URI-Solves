7fbb2bf13c47acf746d94b6a2636bf19326e3c66
Mehrab Mohsin Khan <47208766+Abeshkhan@users.noreply.github.com>
2019-02-24 16:05:06 +0600

    Add files via upload

---
+++ uri_1003.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class uri_1003{
+  public static void main(String args[]){
+    Scanner abesh=new Scanner (System.in);
+    int A=abesh.nextInt();
+    int B=abesh.nextInt();
+    int SOMA = A+B;
+    System.out.println("SOMA = "+SOMA);
+  }}
