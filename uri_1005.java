f7992b0c6a90a195150e91c97bfad6f27fe94c9d
Mehrab Mohsin Khan <47208766+Abeshkhan@users.noreply.github.com>
2019-02-24 17:03:31 +0600

    Update uri_1005.java

--- uri_1005.java
+++ uri_1005.java
@@ -4,13 +4,13 @@ public class URI_1005 {
 
     public static void main(String[] args){
 
-            float A, B, med;
-            Scanner sc = new Scanner(System.in);
-            A = sc.nextFloat();
-            B = sc.nextFloat();
-            med = (float) (((A * 3.5) + (B * 7.5)) / (3.5 + 7.5));
+            float A, B;
+            Scanner abesh = new Scanner(System.in);
+            A = abesh.nextFloat();
+            B = abesh.nextFloat();
+            double yo = (((A * 3.5) + (B * 7.5)) / (3.5 + 7.5));
             
-            System.out.print("MEDIA=%.5f",med);
+            System.out.printf("MEDIA = %.5f",yo);
             System.out.println();
 
     }
