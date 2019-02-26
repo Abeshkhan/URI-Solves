320fdefe526e95829f5d44a36b0192261832953a
Mehrab Mohsin Khan <47208766+Abeshkhan@users.noreply.github.com>
2019-02-27 02:38:36 +0600

    Add files via upload

---
+++ uri_1007.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class uri_1007{
+  public static void main(String args[]){
+    Scanner abesh= new Scanner ( System.in);
+    int A=abesh.nextInt();
+    int B=abesh.nextInt();
+    int C= abesh.nextInt();
+    int D=abesh.nextInt();
+    int diff = (A*B-C*D);
+    System.out.println("DIFERENCA = " + diff);
+  }}
+    
